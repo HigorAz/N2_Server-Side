633128824458f863d546e5af30cd4fedc42da535
HigorAz <higor.azevedo@catolicasc.edu.br>
2023-10-24 19:04:23 -0300

    Att+Description
    Atualização do código e início dos comentários explicando o código

--- main.py
+++ main.py
@@ -7,50 +7,50 @@ import ujson #Biblioteca "estilziada" para trabalhar com dados em JSON
 from flask import Flask, render_template, request, flash, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates') # integra a pasta dos templates, torna a rota padrão
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db' # conexao com o banco de dados
 app.config['SECRET_KEY'] = "random string" # para criptografar as sessões
 # desde que você tem as classes relaciandas ao banco, o ORM faz o processo de criar, recuperar e converter
 db = SQLAlchemy(app)
 
-
+# abre a rota do indice na rota padrão, que direcionará as funções criadas
 @app.route('/')
 def indice():
-    return render_template('index.html')
+    return render_template('index.html') #retorna o template criado para o index em html, que contém os redirecionamentos
 
-
+#Função para ordenação utilizada, ordenação padrão do Python, que utiliza o método TimSort
 def sort(vetor):
-    x = sorted(vetor)
-    return x
+    x = sorted(vetor) #auxiliar x recebe o valor ordenado do vetor
+    return x #Retorna o valor ordenado
 
-
+#Rota para o problema de ordenação
 @app.route('/ordenacao', methods=['GET', 'POST'])
 def ordena():
-    resultado = ""
-    if request.method == 'POST':
+    resultado = "" #Resultado recebe um valor sem valor para que consiga receber o valor do if e retornar fora do if
+    if request.method == 'POST': #Verifica se recebeu um valor no form do html.
         vetor_ordenacao = request.form["vetor"]
-        if not vetor_ordenacao:
+        if not vetor_ordenacao: #Se não recebeu, retorna erro
             flash("Nada digitado!", "Error")
-        else:
-            vetor = list(map(int, vetor_ordenacao.split(",")))
-            inicio = time.time()
-            ordenado = sort(vetor)
-            fim = time.time()
+        else: #Se receber, entra no algoritmo:
+            vetor = list(map(int, vetor_ordenacao.split(","))) #Faz a separação do vetor pelo delimitador ","
+            inicio = time.time() #Inicia o tempo do cronômetro da função time
+            ordenado = sort(vetor) #Utiliza a função sort declarada anteriormente para ordenar o vetor digitado
+            fim = time.time() #Finaliza o temporizador da função time
 
-            retorno = ujson.dumps(ordenado)
-            tempo_total = ((fim - inicio) * 1000)
-            flash(f"Tempo: {tempo_total}\n{retorno}", "Sucesso")
-            resultado = str(tempo_total) + '  ' + retorno
-    return render_template('ordena.html', resultado = resultado)
+            retorno = ujson.dumps(ordenado) #Transforma o valor do vetor ordenado em json
+            tempo_total = ((fim - inicio) * 1000) #Calcula o tempo do temporizador inciado e finalizado
+            flash(f"Tempo: {tempo_total}\n{retorno}", "Sucesso") # retorna mensagem de sucesso
+            resultado = str(tempo_total) + '  ' + retorno #Concatena o resultado a ser apresentado no return
+    return render_template('ordena.html', resultado = resultado) #Retorna o template em html de ordenação e o resultado declarado anteriormente
 
 
-class Users(db.Model):
+class Users(db.Model): #Declara classe do banco de dados
     # definição dos tipos de dados do banco
-    id = db.Column('user_id', db.Integer, primary_key = True)
-    nome = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    telefone = db.Column(db.Integer)
-    observacao = db.Column(db.String(100))
+    id = db.Column('user_id', db.Integer, primary_key = True) #Coluna de id como chave primária e inteiro
+    nome = db.Column(db.String(100)) #Coluna de nome como string de 100 caracteres
+    email = db.Column(db.String(100)) #Coluna de email como string de 100 caracteres
+    telefone = db.Column(db.Integer) #Coluna de telefone como inteiro
+    observacao = db.Column(db.String(100)) #Coluna de observação como string de 100 caracteres
 
     # objeto
     def __init__(self, nome, email, telefone, observacao):
